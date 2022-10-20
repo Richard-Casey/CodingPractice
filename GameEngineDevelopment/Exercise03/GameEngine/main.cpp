abad620dcc70cd238acfcf3d246fdf6e371cd665
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2022-10-20 15:51:54 +0100

    homepm1

--- GameEngineDevelopment/Exercise03/GameEngine/main.cpp
+++ GameEngineDevelopment/Exercise03/GameEngine/main.cpp
@@ -1,19 +1,53 @@
-#include "SDL.h"
 #include "game.h"
 #include "input.h"
 
-// PAGE 16 OF TUTORIAL 01!
+#undef main
 
 int main(int argc, char* argv[])
 {
-	Game* game = new Game();				// Create a new game object
+	Game* game = new Game();			// Create a new game object
+	input* input = new input();			// Create an Input object
 
-		if (game)
+	if (game && input)
+	{
+		// Decalre and initialise variables used for colour background
+		Uint8 r = 127, g = 127, b = 127, a = 255;
+
+		// Check keys pressed to update variable
+		while (!input->KeyIsPressed(KEY_ESCAPE))
 		{
-			game->SetDisplayColour();	// Set and show our coloured display
-			delete game;				// clean up
-			game = nullptr;
+			input->Update();
+
+			// Incread r
+			if (input->KeyIsPressed(KEY_R))
+			{
+				if (++r > 255) r = 0;
+			}
+
+			// Increase g
+			if (input->KeyIsPressed(KEY_G))
+			{
+				if (++g > 255) g = 0;
+			}
+
+			// Increase b
+			if (input->KeyIsPressed(KEY_B))
+			{
+				if (++b > 255) b = 0;
+			}
+
+			game->SetDisplayColour(r, g, b, a);				// Set our coloured display
+			game->Update();									// Update the game
 		}
 
-	return 0;							// Exit nicely
+		delete input;			// Clean Up
+		input = nullptr;
+
+		delete game;			// Clean Up
+		game = nullptr;
+
+	}
+
+	return 0;			// Exit nicely
+
 }
